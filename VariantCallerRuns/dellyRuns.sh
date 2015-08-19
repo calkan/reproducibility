c48cefe28f9267bc986fbed4ef6a2cd45bbdc94f
canfirtina <canfirtina@gmail.com>
2015-08-20 01:01:11 +0300

    output fixes

--- VariantCallerRuns/dellyRuns.sh
+++ VariantCallerRuns/dellyRuns.sh
@@ -5,8 +5,16 @@ REFFILE=/mnt/compgen/inhouse/share/gatk_bundle/2.8/b37/human_g1k_v37.fasta
 
 for i in `ls $BAMDIR/*.bam`; do
 	fname=`basename $i`
-	$DELLYBIN -t TRA -g $REFFILE $BAMDIR/$fname | cut -f 1,2,4,5,6,7,8,9,10 > $OUTDIR/"$(echo "$fname" | sed s/".bam"/".tra.vcf"/)"
-	$DELLYBIN -t INV -g $REFFILE $BAMDIR/$fname | cut -f 1,2,4,5,6,7,8,9,10 > $OUTDIR/"$(echo "$fname" | sed s/".bam"/".inv.vcf"/)"
-	$DELLYBIN -t DUP -g $REFFILE $BAMDIR/$fname | cut -f 1,2,4,5,6,7,8,9,10 > $OUTDIR/"$(echo "$fname" | sed s/".bam"/".dup.vcf"/)"
-	$DELLYBIN -t DEL -g $REFFILE $BAMDIR/$fname | cut -f 1,2,4,5,6,7,8,9,10 > $OUTDIR/"$(echo "$fname" | sed s/".bam"/".del.vcf"/)"
+	$DELLYBIN -t TRA -o $OUTDIR/"$(echo "$fname" | sed s/".bam"/".tra.vcf"/)" -g $REFFILE $BAMDIR/$fname | cut -f 1,2,4,5,6,7,8,9,10 > 
+	$DELLYBIN -t INV -o $OUTDIR/"$(echo "$fname" | sed s/".bam"/".inv.vcf"/)" -g $REFFILE $BAMDIR/$fname | cut -f 1,2,4,5,6,7,8,9,10 > 
+	$DELLYBIN -t DUP -o $OUTDIR/"$(echo "$fname" | sed s/".bam"/".dup.vcf"/)" -g $REFFILE $BAMDIR/$fname | cut -f 1,2,4,5,6,7,8,9,10 > 
+	$DELLYBIN -t DEL -o $OUTDIR/"$(echo "$fname" | sed s/".bam"/".del.vcf"/)" -g $REFFILE $BAMDIR/$fname | cut -f 1,2,4,5,6,7,8,9,10 > 
+	cat $OUTDIR/"$(echo "$fname" | sed s/".bam"/".tra.vcf"/)" | cut -f 1,2,4,5,6,7,8,9,10 > $OUTDIR/"$(echo "$fname" | sed s/".bam"/".tra.noid.vcf"/)"
+	cat $OUTDIR/"$(echo "$fname" | sed s/".bam"/".inv.vcf"/)" | cut -f 1,2,4,5,6,7,8,9,10 > $OUTDIR/"$(echo "$fname" | sed s/".bam"/".inv.noid.vcf"/)"
+	cat $OUTDIR/"$(echo "$fname" | sed s/".bam"/".dup.vcf"/)" | cut -f 1,2,4,5,6,7,8,9,10 > $OUTDIR/"$(echo "$fname" | sed s/".bam"/".dup.noid.vcf"/)"
+	cat $OUTDIR/"$(echo "$fname" | sed s/".bam"/".del.vcf"/)" | cut -f 1,2,4,5,6,7,8,9,10 > $OUTDIR/"$(echo "$fname" | sed s/".bam"/".del.noid.vcf"/)"
+	rm -f $OUTDIR/"$(echo "$fname" | sed s/".bam"/".tra.vcf"/)"
+	rm -f $OUTDIR/"$(echo "$fname" | sed s/".bam"/".inv.vcf"/)"
+	rm -f $OUTDIR/"$(echo "$fname" | sed s/".bam"/".dup.vcf"/)"
+	rm -f $OUTDIR/"$(echo "$fname" | sed s/".bam"/".del.vcf"/)"
 done;
--- VariantCallerRuns/platypusRuns.sh
+++ VariantCallerRuns/platypusRuns.sh
@@ -5,5 +5,7 @@ REFFILE=/mnt/compgen/inhouse/share/gatk_bundle/2.8/b37/human_g1k_v37.fasta
 
 for i in `ls $BAMDIR/*bam`; do
 	fname=`basename $i`
-	python $PLATYPUSLOC callVariants --bamFiles=$BAMDIR/$fname --refFile=$REFFILE > grep "#\|PASS" > $OUTDIR/"$(echo $fname | sed s/".bam"/".vcf"/)"
+	python $PLATYPUSLOC callVariants --bamFiles=$BAMDIR/$fname --refFile=$REFFILE --output=$OUTDIR/"$(echo $fname | sed s/".bam"/".vcf.tmp"/)"
+	cat $OUTDIR/"$(echo $fname | sed s/".bam"/".vcf.tmp"/)" | grep "#\|PASS" > $OUTDIR/"$(echo $fname | sed s/".bam"/".vcf"/)"
+	rm -f $OUTDIR/"$(echo $fname | sed s/".bam"/".vcf.tmp"/)"
 done;
